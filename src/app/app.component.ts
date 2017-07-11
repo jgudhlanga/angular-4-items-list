1cf604b70b6ed512bcad93cee504394179c2c53a
James Gudhlanga <jimmyneds@gmail.com>
2017-07-11 08:59:47 +0200

    Upload from the dev server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items = [
+      {name: 'Angular 4', itemDate: new Date(2016, 1, 1)},
+      {name: 'Laravel', itemDate: new Date(2010, 1, 1)},
+      {name: 'Zend Framework', itemDate: new Date(2009, 1, 1)},
+  ]
+  newItem = "";
+  pushItem = function(){
+      if(this.newItem !+ ""){
+          this.items.push({name:this.newItem, itemDate: new Date()});
+          this.newItem = "";
+      }
+  }
+
+  removeItem = function(index){
+      this.items.splice(index, 1);//remove only one item starting from index given
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
+import {SortPipe} from './app.sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    SortPipe,
+    AppComponent,
+    ProductComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+        {path: 'members', component: MembersComponent},
+        {path: 'products', component: ProductComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
